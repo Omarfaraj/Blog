a5591cba623cf2d584cb74201a96eea3e0760e1a
blvck <omarfaraj355@gmail.com>
2024-03-07 21:56:08 +0300

    styling

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,21 @@
+import { useState } from 'react';
 
-
-const Home = () => { 
-    let name = 'faraj'
-     
-    const handleClick = () => {
-        console.log('Hello!')
-    }
+const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'my website', body: 'loren ipsum...', author: 'faraj', id: 1},
+        { title: 'welcome party!', body: 'loren ipsum...', author: 'Omar', id: 2},
+        { title: 'web dev top tips', body: 'loren ipsum...', author: 'Zakariya', id: 3}
+    ])
 
     return (
         <div className="home">
             <h2>Homepage</h2>
-            <p>{ name }</p>
-            <button onClick={(handleClick)}>Click me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blogs.id}>
+                <h2>{ blog.title }</h2>
+                <p>Written by { blog.author }</p>
+            </div>
+            ))}
         </div>
     );
 }
