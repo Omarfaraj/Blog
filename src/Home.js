706021c2d8e4ef8f8eb7e7fe9902c61d4cf51637
blvck <omarfaraj355@gmail.com>
2024-03-10 22:47:51 +0300

    small changes

--- src/Home.js
+++ src/Home.js
@@ -2,31 +2,28 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'my website', body: 'loren ipsum...', author: 'faraj', id: 1},
-        { title: 'welcome party!', body: 'loren ipsum...', author: 'Omar', id: 2},
-        { title: 'web dev top tips', body: 'loren ipsum...', author: 'Zakariya', id: 3}
-    ])
+    const [blogs, setBlogs] = useState(null)
 
-    const [name, setName] = useState('faraj');
+    
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(name);
     }
 
     useEffect(() => {
-        console.log('use effect ran');
-        console.log(blogs);
-    }, [name]);
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data);
+            console.log(blogs);
+        })
+    }, []);
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete}/>
-            <button onClick={() => setName('Omar')}>change name</button>
-            <p>{ name }</p>
+            {blogs && <BlogList blogs={blogs} title="All blogs!" />}
         </div>
     );
-}
+
  
 export default Home;
