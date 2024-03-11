2ab95812da6194cef1efc0940efacbd2fc24d495
blvck <omarfaraj355@gmail.com>
2024-03-11 22:39:33 +0300

    changes

--- src/Home.js
+++ src/Home.js
@@ -2,28 +2,45 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
-
-    
-
-    }
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            setBlogs(data);
-            console.log(blogs);
-        })
-    }, []);
+        setTimeout(() => {
+            const fetchData = async () => {
+                try {
+                    const response = await fetch('http://localhost:8000/blogs');
+                    const data = await response.json();
+                    setBlogs(data);
+                    setIsPending(false);
+                     
+                    
+                } catch (error) {
+                    console.error('Error fetching data:', error);
+                }
+            };
+    
+            fetchData();
+        }, 1000)
+         // Call fetchData outside of useEffect
+
+    }, []); // Make sure to pass an empty dependency array to run useEffect only once
+
+    //         ('http://localhost:8000/blogs')
+    //         .then(res => res.json()
+    //         )
+    //         .then(data => {
+    //             setBlogs(data);
+    //             console.log(blogs);
+    //         });
+    // }, [blogs]);
 
     return (
         <div className="home">
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All blogs!" />}
         </div>
     );
+};
 
- 
 export default Home;
